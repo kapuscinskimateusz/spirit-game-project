f1257b6f613145f14da78ad48ac0fce31bcacaef
kapuscinskimateusz <kapuscinski.mateusz01@gmail.com>
2021-02-01 19:42:32 +0100

    Add preload and menu scene

---
+++ Spirit-Game Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject gameplayObjects;
+
+    private void Awake()
+    {
+        gameplayObjects = GameObject.Find("GameplayObjects");
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log(scene.name);
+
+        if (scene.name == "Menu")
+        {
+            gameplayObjects.SetActive(false);
+        }
+        else
+            gameplayObjects.SetActive(true);
+    }
+}
